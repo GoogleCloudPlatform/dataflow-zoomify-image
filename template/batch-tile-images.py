4709113dfcf72deaa5b595706e967e4c1fb2f889
regiov <renato@cria.org.br>
2021-10-19 18:13:27 -0300

    Save ImageProperties.xml

--- template/batch-tile-images.py
+++ template/batch-tile-images.py
@@ -21,6 +21,7 @@ import logging
 import math
 import itertools
 from concurrent.futures import ThreadPoolExecutor
+from abc import ABC, abstractmethod
 
 import imageio
 import requests
@@ -35,7 +36,7 @@ from apache_beam.transforms.core import ParDo
 from google.cloud import storage
 
 
-class UploadGCS(beam.DoFn):
+class DoFnWithGCSClient(ABC, beam.DoFn):
     def setup(self):
         self.client = storage.Client()
         adapter = requests.adapters.HTTPAdapter(pool_connections=128, pool_maxsize=128, max_retries=3, pool_block=True)
@@ -43,56 +44,83 @@ class UploadGCS(beam.DoFn):
         self.client._http._auth_request.session.mount("https://", adapter)
     def start_bundle(self):
         self.executor = ThreadPoolExecutor(128)
+    @abstractmethod
     def process(self, el):
-        client = self.client
+        pass
+    def finish_bundle(self):
+        self.executor.shutdown()
+
+class UploadGCS(DoFnWithGCSClient):
+    def process(self, el):
         path = el[0]
         data = el[1]
         bucket_name = path.replace("gs://", "").split("/")[0]
-        bucket = client.bucket(bucket_name)
+        bucket = self.client.bucket(bucket_name)
         target_key = path.replace(f"gs://{bucket_name}/","")
         blob = bucket.blob(target_key)
-        try:   
+        try:
             self.executor.submit(blob.upload_from_string, data)
         except Exception as e:
-                print(e)
-        return "Ok"
-    def finish_bundle(self):
-        self.executor.shutdown()
+            print(e)
+        return ["Ok"]
+
+class ProcessTiles(DoFnWithGCSClient):
+    def process(self, el):
+        im = el["image"]
+        output = el["output"]
+        file_name = el["name"]
+        tile_size = 256
+        num_tiles = 0
+        tier = math.ceil(max([math.log(shape/tile_size, 2) for shape in im.shape]))
+        max_step = 2 ** tier
+        steps = [int(max_step/2**i) for i in range(tier + 1)]
+        tiers = [im[::step, ::step] for step in steps]
+        def next_i():
+            i = 0
+            while i < tile_size:
+                yield i // tile_size
+                i += 1
+        tile_group = next_i()
+        for tier_number, tier in enumerate(tiers):
+            for item in itertools.product([i for i in range(math.ceil(tier.shape[0]/tile_size))], [i for i in range(math.ceil(tier.shape[1]/tile_size))]):
+                num_tiles += 1
+                yield{
+                    "tier": tier,
+                    "tier_number": tier_number,
+                    "item": item,
+                    "tile_group": next(tile_group),
+                    "output": output,
+                    "name": file_name
+                }
+        # Save ImageProperties.xml
+        img_width = im.shape[1]
+        img_height = im.shape[0]
+        data = f'<IMAGE_PROPERTIES WIDTH="{img_width}" HEIGHT="{img_height}" NUMTILES="{num_tiles}" NUMIMAGES="1" VERSION="1.8" TILESIZE="{tile_size}"/>'
+        path = f"{output}{file_name}/ImageProperties.xml"
+        bucket_name = path.replace("gs://", "").split("/")[0]
+        bucket = self.client.bucket(bucket_name)
+        target_key = path.replace(f"gs://{bucket_name}/","")
+        blob = bucket.blob(target_key)
+        try:
+            self.executor.submit(blob.upload_from_string, data)
+        except Exception as e:
+            print(e)
+
 
 def by_extension(el, extensions):
   ext = el.path.split('.')[-1]
   return ext in extensions
 
-def img_read(path):
+def img_read(path, output):
     kwargs = {}
     ext = path.split('.')[-1]
     if ext == 'png':
         kwargs['pilmode'] = 'RGB' # discard alpha channel
     gcs = gcsio.GcsIO()
-    return {"image": imageio.imread(gcs.open(path), **kwargs), "name": path.split("/")[-1]}
-
-def process_tiles(el, output):
-    im = el["image"]    
-    tier = math.ceil(max([math.log(shape/256, 2) for shape in im.shape]))
-    max_step = 2 ** tier
-    steps = [int(max_step/2**i) for i in range(tier + 1)]
-    tiers = [im[::step, ::step] for step in steps]
-    def next_i():
-        i = 0
-        while i < 256:
-            yield i // 256
-            i += 1
-    tile_group = next_i()
-    for tier_number, tier in enumerate(tiers):
-        for item in itertools.product([i for i in range(math.ceil(tier.shape[0]/256))], [i for i in range(math.ceil(tier.shape[1]/256))]):
-            yield{
-                "tier": tier,
-                "tier_number": tier_number,
-                "item": item,
-                "tile_group": next(tile_group),
-                "output": output+el["name"],
-                "name": el["name"]
-            }
+    return {"image": imageio.imread(gcs.open(path), **kwargs),
+            "name": path.split("/")[-1],
+            "output": output
+           }
 
 def save_tile(el):
     # gcs = gcsio.GcsIO()
@@ -100,7 +128,7 @@ def save_tile(el):
     tier_number = el["tier_number"]
     item = el["item"]
     tile_group = el["tile_group"]
-    output = el["output"]
+    output = el["output"] + el["name"]
     path = f"{output}/TileGroup{tile_group}/{tier_number}-{item[1]}-{item[0]}.jpg"
 
     f = io.BytesIO()
@@ -147,8 +175,8 @@ def main(argv=None, save_main_session=True):
     (
         files
         | 'Filter by file extension' >> beam.Filter(by_extension, valid_extensions)
-        | 'Read images to arrays' >> beam.Map(lambda x: img_read(x.path))
-        | 'Tile images' >> beam.ParDo(process_tiles, known_args.output)
+        | 'Read images to arrays' >> beam.Map(lambda x: img_read(x.path, known_args.output))
+        | 'Tile images' >> beam.ParDo(ProcessTiles())
         | 'Save tile' >> beam.ParDo(save_tile)
         | 'Upload to GCS' >> beam.ParDo(UploadGCS())
     )
